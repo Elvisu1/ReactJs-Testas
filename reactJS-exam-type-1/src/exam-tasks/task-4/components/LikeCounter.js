6437b1cddabe0de57bc31f9b9e0e295b57749061
Elvis <elvinasu@gmail.com>
2021-11-19 18:30:35 +0200

    like dislike work separately

--- reactJS-exam-type-1/src/exam-tasks/task-4/components/LikeCounter.js
+++ reactJS-exam-type-1/src/exam-tasks/task-4/components/LikeCounter.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import css from './LikeCounter.module.css'
 
 function LikeCounter() {
+    const [likeValue, setLikeValue] = useState(0);
+    const handleIncrement = () => {
+        setLikeValue(likeValue + 1)
+    }
+    const[dislikeValue, setDislikeValue] = useState(0)
+    const handleDecrement = () => {
+        setDislikeValue(dislikeValue - 1)
+    }
     return (
-        <div>
-            <button><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
-            <button><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+        <div className={css.div}>
+            <button onClick={handleIncrement} className={css.button}><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+            <h2>{likeValue}</h2>
+            <button onClick={handleDecrement} className={css.button}><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+            <h2>{dislikeValue}</h2>
         </div>
     )
 }
