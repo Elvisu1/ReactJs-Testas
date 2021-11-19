e1d18824f5cb5dc333072a8fe36a7c57f5609e13
Elvis <elvinasu@gmail.com>
2021-11-19 18:06:16 +0200

    like dislike buttons

--- reactJS-exam-type-1/src/exam-tasks/task-4/Task4.js
+++ reactJS-exam-type-1/src/exam-tasks/task-4/Task4.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LikeCounter from "./components/LikeCounter";
 
 function Task4() {
   return (
     <div>
       <h3>Task 4</h3>
+        <LikeCounter/>
     </div>
   );
 }
@@ -13,9 +15,9 @@ export default Task4;
 /* Task 4
 Atkurkite šį komponentą: https://prnt.sc/1296zmk
 
-Pastabos, leidžiama balsuoti, tik vieną kartą. 
-Paspaudus ant vieno mygtuko, antrą kartą paspausti ant jo 
-negalima, tačiau leidžiama pakeisti nuomonę ir paspausti 
+Pastabos, leidžiama balsuoti, tik vieną kartą.
+Paspaudus ant vieno mygtuko, antrą kartą paspausti ant jo
+negalima, tačiau leidžiama pakeisti nuomonę ir paspausti
 ant kito. Tokiu atveju nuo prieš tai paspausto balsas "nuimamas"
 ir perduodamas kitam.
 
---
+++ reactJS-exam-type-1/src/exam-tasks/task-4/components/LikeCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LikeCounter() {
+    return (
+        <div>
+            <button><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+            <button><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+        </div>
+    )
+}
+export default LikeCounter
