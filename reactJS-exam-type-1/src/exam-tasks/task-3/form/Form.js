53efedb8de799bfae71b2b2c2fe312b1c199ac5c
Elvis <elvinasu@gmail.com>
2021-11-19 15:12:13 +0200

    form submiting working

--- reactJS-exam-type-1/src/exam-tasks/task-3/form/Form.js
+++ reactJS-exam-type-1/src/exam-tasks/task-3/form/Form.js
@@ -1,21 +1,44 @@
-import React, {useState} from "react";
+import {useState} from "react";
+
 function Form(){
+    const [values, setValues] = useState({
+        name: "",
+        surname: "",
+        age: "",
+    });
+    const [submitted, setSubmitted ] = useState(false)
+    const handleName = (event) =>{
+        setValues({...values, name: event.target.value})
+    }
+    const handleSurname = (event) =>{
+        setValues({...values, surname: event.target.value})
+    }
+    const handleAge = (event) =>{
+        setValues({...values, age: event.target.value})
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSubmitted(true);
+    }
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
+
             <div>
                 <label>Name</label>
-                <input type="text" required />
+                <input onChange={handleName} value={values.name} type="text" required />
             </div>
             <div>
                 <label>Surname</label>
-                <input type="text" required />
+                <input onChange={handleSurname} value={values.surname} type="text" required />
             </div>
             <div>
                 <label>Age</label>
-                <input type="number" required />
+                <input onChange={handleAge} value={values.age} type="number" required />
             </div>
             <input type="submit" value="Submit"/>
-
+            {submitted ? <div>
+                Dėkojame, kad užsiregistravote
+            </div> : null}
         </form>
     )
 }
